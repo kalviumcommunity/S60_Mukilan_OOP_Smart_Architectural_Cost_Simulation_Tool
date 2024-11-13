b083d5c0ccc72c279a39e0a23f5939dae4f4caa4
Mukilan Palanichamy <mukilan.p@kalvium.community>
2024-11-13 21:38:33 +0530

    added Dynamic allocation (New and Delete)

--- SmartArchitecturalCostSimulator.cpp
+++ SmartArchitecturalCostSimulator.cpp
@@ -8,16 +8,15 @@ private:
     double area;
 
 public:
-   
+
     Building(string name, double area) {
         this->name = name;
         this->area = area;
     }
 
-   
     Building& setArea(double newArea) {
         this->area = newArea;
-        return *this;  
+        return *this;
     }
 
 
@@ -29,6 +28,11 @@ public:
     void displayInfo() {
         cout << "Building Name: " << name << endl;
         cout << "Area: " << area << " sqm" << endl;
+    }
+
+
+    double getArea() const {
+        return this->area;
     }
 };
 
@@ -44,10 +48,9 @@ public:
         this->costPerSqm = costPerSqm;
     }
 
-
     Material& setCostPerSqm(double newCost) {
         this->costPerSqm = newCost;
-        return *this;  
+        return *this;
     }
 
 
@@ -62,25 +65,60 @@ public:
     }
 };
 
+class EnergySimulationEngine {
+private:
+    double energyConsumption; 
+
+public:
+
+    EnergySimulationEngine(double consumption) {
+        this->energyConsumption = consumption;
+    }
+
+
+    double calculateEnergyEfficiency(double area) {
+        return energyConsumption * area; 
+    }
+
+
+    void displayEnergyEfficiency(double area) {
+        double totalEnergy = calculateEnergyEfficiency(area);
+        cout << "Total Energy Consumption: " << totalEnergy << " kWh for " << area << " sqm" << endl;
+    }
+};
+
 int main() {
 
-    Building buildings[] = {
-        Building("Office Complex", 500.0),
-        Building("Residential Apartment", 300.0),
-        Building("Shopping Mall", 800.0)
-    };
+
+    Building* buildings[3];
+    buildings[0] = new Building("Office Complex", 500.0);
+    buildings[1] = new Building("Residential Apartment", 300.0);
+    buildings[2] = new Building("Shopping Mall", 800.0);
+
+   
+    Material* concrete = new Material("Concrete", 150.0);
+    EnergySimulationEngine* energySim = new EnergySimulationEngine(12.5); 
 
 
-    Material concrete("Concrete", 150.0);
+    concrete->displayInfo();
 
-
-    concrete.displayInfo();
 
     for (int i = 0; i < 3; ++i) {
         cout << "\nDetails for Building " << (i + 1) << ":\n";
-        buildings[i].displayInfo();
-        double totalCost = buildings[i].calculateCost(concrete.getCostPerSqm());
+        buildings[i]->displayInfo();
+        
+
+        double totalCost = buildings[i]->calculateCost(concrete->getCostPerSqm());
         cout << "Total Construction Cost: $" << totalCost << endl;
+
+
+        energySim->displayEnergyEfficiency(buildings[i]->getArea());  
+    }
+
+    delete concrete;
+    delete energySim;
+    for (int i = 0; i < 3; ++i) {
+        delete buildings[i];
     }
 
     return 0;
